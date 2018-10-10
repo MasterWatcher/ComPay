97f9cb1675f65bf865eb0b16646863b722cfccfb
Anton Goncharov <antongoncharov4@gmail.com>
2018-10-10 21:24:39 +0300

    Displaing total on result screen

--- ComPay/ViewControllers/Result/ResultViewContoller.swift
+++ ComPay/ViewControllers/Result/ResultViewContoller.swift
@@ -13,19 +13,33 @@ import NSObject_Rx
 
 class ResultViewContoller: UIViewController, BindableType {
     
+    @IBOutlet weak var hotWaterLabel: UILabel!
+    @IBOutlet weak var coldWaterLabel: UILabel!
+    @IBOutlet weak var electricityLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var containerView: UIStackView!
+    @IBOutlet weak var indicator: UIActivityIndicatorView!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
     
     var viewModel: ResultViewModel!
     
     func bindViewModel() {
-        let input = ResultViewModel.Input(cancelTrigger: cancelButton.rx.tap.asDriver())
+        let viewWillAppear = rx.sentMessage(#selector(UIViewController.viewWillAppear(_:)))
+            .mapToVoid()
+            .asDriverOnErrorJustComplete()
+        let input = ResultViewModel.Input(loadDataTrigger: viewWillAppear, cancelTrigger: cancelButton.rx.tap.asDriver())
         let output = viewModel.transform(input: input)
+        output.isLoading
+            .drive(indicator.rx.isAnimating)
+            .disposed(by: rx.disposeBag)
+        output.isLoading
+            .drive(containerView.rx.isHidden)
+            .disposed(by: rx.disposeBag)
+        output.total
+            .drive(totalLabel.rx.text)
+            .disposed(by: rx.disposeBag)
         output.dismiss
             .drive()
-        .disposed(by: rx.disposeBag)
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+            .disposed(by: rx.disposeBag)
     }
 }
--- ComPay/ViewControllers/Result/ResultViewModel.swift
+++ ComPay/ViewControllers/Result/ResultViewModel.swift
@@ -13,10 +13,13 @@ import RxCocoa
 struct ResultViewModel: ViewModel {
     
     struct Input {
+        let loadDataTrigger: Driver<Void>
         let cancelTrigger: Driver<Void>
     }
     
     struct Output {
+        let isLoading: Driver<Bool>
+        let total: Driver<String>
         let dismiss: Driver<Void>
     }
     
@@ -29,10 +32,23 @@ struct ResultViewModel: ViewModel {
     let coordinator: SceneCoordinator
     
     func transform(input: Input) -> Output {
+        let activityIndicator = ActivityIndicator()
+        let isLoading = activityIndicator.asDriver()
+        let lastItem = input.loadDataTrigger
+            .flatMapLatest {_ in
+                return self.service.monthData()
+                    .trackActivity(activityIndicator)
+                    .asDriverOnErrorJustComplete()
+                    .map { $0.last }
+        }
+        
+        let total = lastItem
+            .map { "\($0?.value ?? 0)" }
+        
         let dismiss = input.cancelTrigger
             .flatMapLatest {_ in
                 return self.coordinator.pop().asDriver(onErrorJustReturn: ())
         }
-        return Output(dismiss: dismiss)
+        return Output(isLoading: isLoading, total: total, dismiss: dismiss)
     }
 }
